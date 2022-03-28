eae7aec377b3daae191e4e8fe0eb86a59f498e1e
devardha <ardhaydhtm@gmail.com>
2022-03-28 12:31:22 +0700

    Finish module 2 session 3

--- src/components/AlbumText.jsx
+++ src/components/AlbumText.jsx
@@ -1,23 +1,23 @@
 const AlbumText = ({ label, data }) => {
-  if (Array.isArray(data)) {
-    return (
-      <p>
-        {label}:
-        {data.map((item, index) => (
-          <span key={index}>
-            {item.name}
-            {data.length > index + 1 && ", "}
-          </span>
-        ))}
-      </p>
-    );
-  }
+	if (Array.isArray(data)) {
+		return (
+			<p>
+				<span>{label}: </span>
+				{data.map((item, index) => (
+					<span key={index}>
+						{item.name}
+						{data.length > index + 1 && ", "}
+					</span>
+				))}
+			</p>
+		);
+	}
 
-  return (
-    <p>
-      {label}: {data}
-    </p>
-  );
+	return (
+		<p>
+			{label}: {data}
+		</p>
+	);
 };
 
 export default AlbumText;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,24 @@
 import AlbumText from "../../components/AlbumText";
 import Button from "../../components/Button";
 import Image from "../../components/Image";
-import song from "../../data/song";
+import songs from "../../data/songs";
 
 const Home = () => {
-  return (
-    <div id="playlist">
-      <Image url={song.album.images[0].url} alt="cover" />
-      <AlbumText label="Title" data={song.name} />
-      <AlbumText label="Artists" data={song.artists} />
-      <AlbumText label="Album" data={song.album.name} />
-      <Button />
-    </div>
-  );
+	return (
+		<div id="playlist">
+			{songs.map((item) => (
+				<div key={item.id} className="song">
+					<Image url={item.album.images[0].url} alt="cover" />
+					<div className="detail">
+						<AlbumText label="Title" data={item.name} />
+						<AlbumText label="Artists" data={item.artists} />
+						<AlbumText label="Album" data={item.album.name} />
+						<Button />
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Home;
